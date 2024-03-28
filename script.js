c3c948e5ada5df317932b06857a4af6e4d5db92f
Andres Tatarnikov <119009469+Andres-Tat@users.noreply.github.com>
2024-03-28 22:36:52 +0300

    add comment

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 'use strict';
 
-
+// Получил доступ к элементам по селектору
 const btnOpenModal = document.querySelectorAll('.show-modal');
 const modalWindow = document.querySelector('.modal');
 const btnCloseModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
+// Использую цикл for of, добавил обработчик событий и функцию, в которой прописано, что должно происходить после срабатывания события
 for (let value of btnOpenModal) {
   value.addEventListener('click', function () {
     overlay.classList.toggle('hidden');
     modalWindow.classList.toggle('hidden');
   });
 }
-
+// Добавил обработчик событий и функцию для кнопки закрытия модального окна, в которой прописано, что должно происходить после срабатывания события
 btnCloseModal.addEventListener('click', function () {
   overlay.classList.toggle('hidden');
   modalWindow.classList.toggle('hidden');
 });
-
+// Добавил обработчик событий и функцию, в которой прописано, что должно происходить после срабатывания события
 overlay.addEventListener('click', function () {
   overlay.classList.toggle('hidden');
   modalWindow.classList.toggle('hidden');
 });
-
+// Добавил обработчик событий и функцию для кнопки 'Escape', в которой прописано, что должно происходить после срабатывания события
 document.addEventListener('keydown', function (event) {
   if (event.key == 'Escape' && !modalWindow.classList.contains('hidden')) {
     overlay.classList.toggle('hidden');
     modalWindow.classList.toggle('hidden');
   }
 });
-
